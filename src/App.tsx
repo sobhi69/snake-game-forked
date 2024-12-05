db5d0274843a6677af8aa6884e2d65c0e085d591
Mohamed Sobhi <mhegazy6371@gmail.com>
2024-12-05 10:54:43 +0200

    i didn't serew it up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { ChakraProvider, Container, Heading } from "@chakra-ui/react";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import CanvasBoard from "./components/CanvasBoard";
 import ScoreCard from "./components/ScoreCard";
 import store from "./store";
 
 const App = () => {
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
       <ChakraProvider>
         <Container maxW="container.lg" centerContent>
           <Heading as="h1" size="xl">SNAKE GAME</Heading>
@@ -14,7 +14,7 @@ const App = () => {
           <CanvasBoard height={600} width={1000} />
         </Container>
       </ChakraProvider>
-    </Provider>
+    {/* </Provider> */}
   );
 };
 
